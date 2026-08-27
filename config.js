ad78c67971018b8ef40c6e9bebbf1997f81c9801
reenaaws07-hash <reena.aws07@gmail.com>
2026-08-27 11:28:36 +0530

    Switch multer storage to memory
    Changed multer storage from disk to memory.

--- config.js
+++ config.js
@@ -1,17 +1,7 @@
-const path = require("path");
 const multer = require("multer");
 
-const uploadDir = path.join(__dirname, "uploads", "jokes");
-
 const upload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-      cb(null, `${Date.now()}-${file.originalname}`);
-    },
-  }),
+  storage: multer.memoryStorage(),
   limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB limit
 });
 
